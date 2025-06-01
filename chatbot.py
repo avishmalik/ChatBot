93186f142e7c150bb9e0a687d158339ea9ecd2a1
Avish Malik <69065857+avishmalik@users.noreply.github.com>
2025-06-01 07:50:36 +0000

    added chatbot file

---
+++ chatbot.py
@@ -0,0 +1,63 @@
+import streamlit as st
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain_community.chat_models import ChatOpenAI
+
+from dotenv import load_dotenv
+import os
+
+
+
+# Load the .env file
+load_dotenv()
+
+# Read environment variables
+open_ai_key = os.getenv("OPENAI_API_KEY")
+
+st.header("Chatbot")
+
+
+with  st.sidebar:
+    st.title("Your Documents")
+    file = st.file_uploader(" Upload a PDf file and start asking questions", type="pdf")
+
+if file is not None:
+    pdf_reader = PdfReader(file)
+    text = ""
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators="\n",
+        chunk_size=1000,
+        chunk_overlap=150,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+
+    embeddings = OpenAIEmbeddings(openai_api_key=open_ai_key)
+
+    vector_store = FAISS.from_texts(chunks, embeddings)
+
+    user_question = st.text_input("Type Your question here")
+
+    # do similarity search
+    if user_question:
+        match = vector_store.similarity_search(user_question)
+
+        llm = ChatOpenAI(
+            openai_api_key = open_ai_key,
+            temperature = 0,
+            max_tokens = 1000,
+            model_name = "gpt-3.5-turbo"
+        )
+
+
+        chain = load_qa_chain(llm, chain_type="stuff")
+
+        response = chain.run(input_documents = match, question = user_question)
+        st.write(response)
+
